17cc8cc909b1b137666036d48daa7fb1a08fe511
M1-shin <sheenblaire@gmail.com>
2025-08-10 01:09:39 +0800

    added registration

--- src/Main/MainClass.java
+++ src/Main/MainClass.java
@@ -1,12 +1,14 @@
 package Main;
 import java.util.Scanner;
 import Banking.BankingClass;
+import java.util.ArrayList;
 
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("");
         Scanner sc = new Scanner(System.in);
         int choice;
+        BankingClass bank = null;
         
         System.out.println("HELLO WELCOME TO THE SYSTEM!");
         System.out.println("1. Banking");
@@ -17,31 +19,89 @@ public class MainClass {
         
         switch(choice){
             case 1:
-                BankingClass bank = new BankingClass();
-                int attempts = 0;
-                boolean verified = false;
+                int attempts = 3;
+                int option, resp;
+                int addMore;
+                ArrayList<BankingClass> accounts = new ArrayList<>(); 
 
-                while(attempts < 3){
-                    System.out.print("Enter your Account No: ");
-                    int accountNo = sc.nextInt();
-                    System.out.print("Enter your Pin: ");
-                    int pin = sc.nextInt();
-                        if(bank.verifyAccount(accountNo, pin)){
-                            System.out.println("Login Successful!");
-                            verified = true;
+                do {
+                    System.out.println("1. Register Account");
+                    System.out.println("2. Login Account");
+                    System.out.println("3. View All Accounts");
+                    System.out.print("Enter Selection: ");
+                    option = sc.nextInt();
+
+                    switch (option) {
+                        case 1: 
+                            do {
+                                bank = new BankingClass();
+                                System.out.print("Enter Account No.: ");
+                                bank.setAccountNo(sc.nextInt());
+                                System.out.print("Enter Account Pin: ");
+                                bank.setPin(sc.nextInt());
+                                accounts.add(bank);
+                                System.out.println("Account registered successfully!");
+                                System.out.print("Do you want to register another account? (1/0): ");
+                                addMore = sc.nextInt();
+                                } while (addMore == 1);
+                                break;
+
+                        case 2:
+                            if (accounts.isEmpty()) {
+                                System.out.println("No account registered yet!");
+                                break;
+                            }
+
+                            System.out.print("Enter your Account No: ");
+                            int accountNo = sc.nextInt();
+                            System.out.print("Enter your Pin: ");
+                            int pin = sc.nextInt();
+
+                            boolean found = false;
+                            for (BankingClass acc : accounts) {
+                                if (acc.verifyAccount(accountNo, pin)) {
+                                    bank = acc;
+                                    found = true;
+                                    break;
+                            }
+                            }
+
+                            if (!found) {
+                                attempts--;
+                                if (attempts == 0) {
+                                    System.out.println("Maximum login attempts reached!");
+                                    System.exit(0);
+                            }
+                                System.out.println("INVALID ACCOUNT! Attempts Left: " + attempts);
+                            }   
+                                else{
+                                System.out.println("Login successful!");
+                            }
                             break;
-                        } else {
-                            System.out.println("Invalid! Please try again.");
-                            attempts++;
-                        }
-                }
-                if(!verified){
-                    System.out.println("Maximum login attempts reached. Access denied.");
-                } 
-                break;
+
+                        case 3:
+                            if (accounts.isEmpty()) {
+                                System.out.println("No account registered yet.");
+                            } else {
+                                System.out.println("\n--- ALL REGISTERED ACCOUNTS ---");
+                                for (BankingClass acc : accounts) {
+                                    System.out.println("Account No: " + acc.getAccountNo());
+                            }
+                            }
+                            break;
+
+                        default:
+                            System.out.println("Invalid option!");
+                            }
+                            System.out.print("Do you want to continue? (1/0): ");
+                            resp = sc.nextInt();
+                            } while (resp == 1);
+                            break;
+                            
             case 2:
                 
                 break;
+                
             case 3:
                 
                 break;
